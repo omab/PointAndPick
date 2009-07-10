b400becf9e7b32315f3027d2c20ab59d3e545139
Matías Aguirre <omab@debianuruguay.org>
2009-07-10 13:49:17 -0300

    Move all inside a class

--- pp.py
+++ pp.py
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import pygtk
 pygtk.require("2.0")
 from gtk import main_quit, Window, WINDOW_TOPLEVEL, Button, DrawingArea, \
-                Label, Table, main
+                Label, Table, main, WINDOW_POPUP
 from gtk.gdk import colormap_get_system, POINTER_MOTION_MASK, \
                     BUTTON_PRESS_MASK, pointer_ungrab, Pixbuf, \
                     COLORSPACE_RGB, get_default_root_window, pointer_grab, \
@@ -40,113 +40,114 @@ def rgb_to_string(rgb):
     return "#" + ''.join('%02x' % c for c in rgb)
 
 
-def pick_stop(*args, **kwargs):
-    """Stop application"""
-    main_quit()
+class PointAndPick(object):
+    def __init__(self, title=TITLE, level=WINDOW_TOPLEVEL, on_exit=None):
+        self.title = title
+        self.grabbing = False
+        self.events = []
+        self.rgb = WHITE_RGB
+        self.on_exit = on_exit
+
+        self.window = Window(level)
+        self.window.set_title(title)
+        self.window.set_resizable(True)
+        self.window.add_events(POINTER_MOTION_MASK)
+        self.window.set_default_size(350, 150)
+        self.colors = []
+
+        grab_btn = Button('Grab')
+        grab_btn.connect_object('clicked', self.toggle_grab, self.window)
+        self.grab_btn = grab_btn
+
+        exit_btn = Button('Exit')
+        exit_btn.connect_object('clicked', self.destroy, self.window)
+
+        drawing = DrawingArea()
+        drawing.connect_object('expose_event', self.do_expose, self.window)
+        self.drawing = drawing
+
+        label = Label(rgb_to_string(WHITE_RGB))
+        self.label = label
+
+        table = Table(2, 2, True)
+        table.attach(self.drawing, 0, 1, 0, 1)
+        table.attach(label,   0, 1, 1, 2)
+        table.attach(grab_btn, 1, 2, 0, 1)
+        table.attach(exit_btn, 1, 2, 1, 2)
+        self.window.add(table)
+
+    def show(self):
+        self.window.show_all()
+
+    def destroy(self, *args, **kwargs):
+        self.window.hide_all()
+        self.window.do_destroy(self.window)
+        if self.on_exit:
+            self.on_exit()
+
+    def toggle_grab(self, *args, **kwargs):
+        """Toggle pointer grabbing"""
+        { True: self.ungrab, False: self.grab }[self.grabbing]()
+
+    def grab(self):
+       """Grab pointer"""
+       if pointer_grab(self.window.window, True, GRAB_MASK) == GRAB_SUCCESS:
+            self.grabbing = True
+            self.grab_btn.set_label('Ungrab')
+
+            e = [ ('motion_notify_event', self.motion_notify_event),
+                  ('button_press_event',  self.button_press_event) ]
+            self.events = [ self.window.connect(n, h) for n, h in e ]
+
+    def ungrab(self):
+        """Ungrab pointer"""
+        pointer_ungrab()
+        while self.events:
+            self.window.disconnect(self.events.pop())
+        self.grabbing = False
+        self.grab_btn.set_label('Grab')
+
+    def do_expose(self, *args, **kwargs):
+        """Expose the window"""
+        gc_obj = self.drawing.window.new_gc()
+        gc_obj.set_foreground(SYS_COLORMAP.alloc_color('black'))
+        self.drawing.window.draw_rectangle(gc_obj, False, 10, 10, 100, 100)
+        self.draw_color()
+
+    def draw_color(self):
+        """Drag the color box with the pixel under the mouse pointer"""
+        gc_obj = self.drawing.window.new_gc()
+        gc_obj.set_foreground(SYS_COLORMAP.alloc_color(rgb_to_string(self.rgb),
+                              True))
+        self.drawing.window.draw_rectangle(gc_obj, True, 11, 11, 99, 99)
+
+    def motion_notify_event(self, win, event):
+        """Mouse motion_notify_event handler"""
+        pixbuf = Pixbuf(COLORSPACE_RGB, False, 8, 1, 1)
+        root = get_default_root_window()
+        xcoord, ycoord = event.get_root_coords()
+        from_draw = pixbuf.get_from_drawable(root, root.get_colormap(),
+                                             int(xcoord), int(ycoord),
+                                             0, 0, 1, 1)
+        pixel = from_draw.get_pixels_array()[0][0]
+        self.rgb = (pixel[0], pixel[1], pixel[2])
+        self.draw_color()
+        self.label.set_label(rgb_to_string(self.rgb).upper())
+
+    def button_press_event(self, *args, **kwargs):
+        """Mouse button_press_event handler"""
+        self.ungrab()
 
 
-def draw_color(win):
-    """Drag the color box with the pixel under the mouse pointer"""
-    gc_obj = win.pick_data['drawing'].window.new_gc()
-    gc_obj.set_foreground(SYS_COLORMAP.\
-            alloc_color(rgb_to_string(win.pick_data['rgb']), True))
-    win.pick_data['drawing'].window.draw_rectangle(gc_obj, True,
-                                                   11, 11, 99, 99)
-
-
-def ungrab(win):
-    """Ungrab mouse"""
-    pointer_ungrab()
-    while win.pick_data['events']:
-        win.disconnect(win.pick_data['events'].pop())
-    win.pick_data['grabbing'] = False
-    win.pick_data['button1'].set_label('Grab')
-
-
-def button_press_event(win, event):
-    """Mouse button_press_event handler"""
-    ungrab(win)
-
-
-def motion_notify_event(win, event):
-    """Mouse motion_notify_event handler"""
-    pixbuf = Pixbuf(COLORSPACE_RGB, False, 8, 1, 1)
-    root = get_default_root_window()
-    xcoord, ycoord = event.get_root_coords()
-    from_draw = pixbuf.get_from_drawable(root, root.get_colormap(),
-                                         int(xcoord), int(ycoord),
-                                         0, 0, 1, 1)
-    pixel = from_draw.get_pixels_array()[0][0]
-    win.pick_data['rgb'] = (pixel[0], pixel[1], pixel[2])
-    draw_color(win)
-    win.pick_data['label'].\
-        set_label(rgb_to_string(win.pick_data['rgb']).upper())
-
-
-def grab_ungrab(win):
-    """Toggle pointer grabbing status"""
-    if not win.pick_data['grabbing'] and \
-       pointer_grab(win.window, True, GRAB_MASK) == GRAB_SUCCESS:
-            win.pick_data['grabbing'] = True
-            win.pick_data['button1'].set_label('Ungrab')
-            e = [ ('motion_notify_event', motion_notify_event),
-                  ('button_press_event', button_press_event) ]
-            win.pick_data['events'] = [ win.connect(n, h) for n, h in e ]
-    else:
-        ungrab(win)
-
-
-def do_expose(win, *args, **kwargs):
-    """Expose the window"""
-    drawing = win.pick_data['drawing']
-    gc_obj = drawing.window.new_gc()
-    gc_obj.set_foreground(SYS_COLORMAP.alloc_color('black'))
-    drawing.window.draw_rectangle(gc_obj, False, 10, 10, 100, 100)
-    draw_color(win)
-
-
-def init_win():
-    """Initialize main window with it's widgets"""
-    window = Window(WINDOW_TOPLEVEL)
-    window.set_title(TITLE)
-    window.connect('destroy', pick_stop)
-    window.add_events(POINTER_MOTION_MASK)
-
-    button1 = Button('Grab')
-    button1.connect_object('clicked', grab_ungrab, window)
-
-    button2 = Button('Exit')
-    button2.connect_object('clicked', pick_stop, window)
-
-    drawing = DrawingArea()
-    drawing.connect_object('expose_event', do_expose, window)
-
-    label = Label(rgb_to_string(WHITE_RGB))
-
-    table = Table(2, 2, True)
-    table.attach(drawing, 0, 1, 0, 1)
-    table.attach(label,   0, 1, 1, 2)
-    table.attach(button1, 1, 2, 0, 1)
-    table.attach(button2, 1, 2, 1, 2)
-    window.add(table)
-
-    for item in (button1, button2, drawing, label, table, window):
-        item.show()
-
-    data = { 'events': [], 'rgb': WHITE_RGB,
-             'grabbing': False, 'button1': button1,
-             'label': label, 'drawing': drawing, }
-    setattr(window, 'pick_data', data)
-
-
-def pick_start():
-    """Start picker"""
-    init_win()
-    main()
+class PointAndPickPopup(PointAndPick):
+    def __init__(self, *args, **kwargs):
+        super(PointAndPickPopup, self).__init__(level=WINDOW_POPUP, *args, **kwargs)
 
 
 if __name__ == '__main__':
+    p = PointAndPickPopup(on_exit=main_quit)
     try:
-        pick_start()
+        p.show()
+        main()
     except KeyboardInterrupt:
-        pick_stop()
+        p.destroy()
