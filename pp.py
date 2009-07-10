34d1202eb510685fd55030154994ee1834ca7d1b
Matías Aguirre <omab@debianuruguay.org>
2009-07-10 10:19:20 -0300

    Import only modules used from gtk and gdk, separate press button event handler

--- pp.py
+++ pp.py
@@ -21,13 +21,18 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import pygtk
 pygtk.require("2.0")
-import gtk
-from gtk import gdk
+from gtk import main_quit, Window, WINDOW_TOPLEVEL, Button, DrawingArea, \
+                Label, Table, main
+from gtk.gdk import colormap_get_system, POINTER_MOTION_MASK, \
+                    BUTTON_PRESS_MASK, pointer_ungrab, Pixbuf, \
+                    COLORSPACE_RGB, get_default_root_window, pointer_grab, \
+                    GRAB_SUCCESS
 
 
 TITLE = 'Point & Pick' 
-SYS_COLORMAP = gdk.colormap_get_system()
+SYS_COLORMAP = colormap_get_system()
 WHITE_RGB = (255, 255, 255)
+GRAB_MASK = POINTER_MOTION_MASK | BUTTON_PRESS_MASK
 
 
 def rgb_to_string(rgb):
@@ -37,7 +42,7 @@ def rgb_to_string(rgb):
 
 def pick_stop(*args, **kwargs):
     """Stop application"""
-    gtk.main_quit()
+    main_quit()
 
 
 def draw_color(win):
@@ -51,43 +56,44 @@ def draw_color(win):
 
 def ungrab(win):
     """Ungrab mouse"""
-    gdk.pointer_ungrab()
+    pointer_ungrab()
     while win.pick_data['events']:
         win.disconnect(win.pick_data['events'].pop())
     win.pick_data['grabbing'] = False
     win.pick_data['button1'].set_label('Grab')
 
 
+def button_press_event(win, event):
+    """Mouse button_press_event handler"""
+    ungrab(win)
+
+
 def motion_notify_event(win, event):
     """Mouse motion_notify_event handler"""
-    if event.type.value_nick == "button-press":
-        ungrab(win)
-    else:
-        pixbuf = gdk.Pixbuf(gdk.COLORSPACE_RGB, False, 8, 1, 1)
-        root = gdk.get_default_root_window()
-        xcoord, ycoord = event.get_root_coords()
-        from_draw = pixbuf.get_from_drawable(root, root.get_colormap(),
-                                             int(xcoord), int(ycoord),
-                                             0, 0, 1, 1)
-        pixel = from_draw.get_pixels_array()[0][0]
-        win.pick_data['rgb'] = (pixel[0], pixel[1], pixel[2])
-        draw_color(win)
-        win.pick_data['label'].\
-            set_label(rgb_to_string(win.pick_data['rgb']).upper())
+    pixbuf = Pixbuf(COLORSPACE_RGB, False, 8, 1, 1)
+    root = get_default_root_window()
+    xcoord, ycoord = event.get_root_coords()
+    from_draw = pixbuf.get_from_drawable(root, root.get_colormap(),
+                                         int(xcoord), int(ycoord),
+                                         0, 0, 1, 1)
+    pixel = from_draw.get_pixels_array()[0][0]
+    win.pick_data['rgb'] = (pixel[0], pixel[1], pixel[2])
+    draw_color(win)
+    win.pick_data['label'].\
+        set_label(rgb_to_string(win.pick_data['rgb']).upper())
 
 
 def grab_ungrab(win):
     """Toggle pointer grabbing status"""
-    if win.pick_data['grabbing']:
-        ungrab(win)
-    else:
-        if gdk.pointer_grab(win.window, True,
-           gdk.POINTER_MOTION_MASK|gdk.BUTTON_PRESS_MASK) == gdk.GRAB_SUCCESS:
+    if not win.pick_data['grabbing'] and \
+       pointer_grab(win.window, True, GRAB_MASK) == GRAB_SUCCESS:
             win.pick_data['grabbing'] = True
             win.pick_data['button1'].set_label('Ungrab')
-            win.pick_data['events'] = [ win.connect(e, motion_notify_event)
-                                         for e in ('motion_notify_event',
-                                                   'button_press_event') ]
+            e = [ ('motion_notify_event', motion_notify_event),
+                  ('button_press_event', button_press_event) ]
+            win.pick_data['events'] = [ win.connect(n, h) for n, h in e ]
+    else:
+        ungrab(win)
 
 
 def do_expose(win, *args, **kwargs):
@@ -101,23 +107,23 @@ def do_expose(win, *args, **kwargs):
 
 def init_win():
     """Initialize main window with it's widgets"""
-    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    window = Window(WINDOW_TOPLEVEL)
     window.set_title(TITLE)
     window.connect('destroy', pick_stop)
-    window.add_events(gdk.POINTER_MOTION_MASK)
+    window.add_events(POINTER_MOTION_MASK)
 
-    button1 = gtk.Button('Grab')
+    button1 = Button('Grab')
     button1.connect_object('clicked', grab_ungrab, window)
 
-    button2 = gtk.Button('Exit')
+    button2 = Button('Exit')
     button2.connect_object('clicked', pick_stop, window)
 
-    drawing = gtk.DrawingArea()
+    drawing = DrawingArea()
     drawing.connect_object('expose_event', do_expose, window)
 
-    label = gtk.Label(rgb_to_string(WHITE_RGB))
+    label = Label(rgb_to_string(WHITE_RGB))
 
-    table = gtk.Table(2, 2, True)
+    table = Table(2, 2, True)
     table.attach(drawing, 0, 1, 0, 1)
     table.attach(label,   0, 1, 1, 2)
     table.attach(button1, 1, 2, 0, 1)
@@ -136,7 +142,7 @@ def init_win():
 def pick_start():
     """Start picker"""
     init_win()
-    gtk.main()
+    main()
 
 
 if __name__ == '__main__':
